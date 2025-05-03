5b58fc4063f21d53038a29a1482fcdcb302628bc
hintsete <hintseteabera@gmail.com>
2025-05-03 11:46:41 +0300

    keep untracked files

--- app/src/main/java/com/site7x24learn/internshipfrontend/data/network/ApiService.kt
+++ app/src/main/java/com/site7x24learn/internshipfrontend/data/network/ApiService.kt
@@ -1,12 +1,17 @@
 package com.site7x24learn.internshipfrontend.data.network
 
+
 import com.site7x24learn.internshipfrontend.data.model.LoginRequest
 import com.site7x24learn.internshipfrontend.data.model.LoginResponse
 import com.site7x24learn.internshipfrontend.data.model.SignupRequest
 import com.site7x24learn.internshipfrontend.data.model.SignupResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface ApiService {
     @POST("api/auth/login")
@@ -14,4 +19,6 @@ interface ApiService {
 
     @POST("api/auth/signup")
     suspend fun signup(@Body request: SignupRequest): Response<SignupResponse>
+
+
 }
