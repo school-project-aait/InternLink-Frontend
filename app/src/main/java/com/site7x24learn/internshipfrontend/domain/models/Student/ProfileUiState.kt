d6525afbe7d59af3fcecd12c547edae327ac5c92
Meklit Shiferaw <167539798+Meklitshiferaw@users.noreply.github.com>
2025-05-10 10:33:41 +0300

    added student profile

---
+++ app/src/main/java/com/site7x24learn/internshipfrontend/domain/models/Student/ProfileUiState.kt
@@ -0,0 +1,11 @@
+package com.site7x24learn.internshipfrontend.domain.models.Student
+
+data class ProfileUiState(
+  val fullName: String = "",
+  val gender: String = "",
+  val email: String = "",
+  val dob: String = "",
+  val contact: String = "",
+  val address: String = "",
+  val password: String = ""
+)
